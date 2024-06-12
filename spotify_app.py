0128edef787e5cfac4b715732e8fab2ad02e0bff
Dante Goss <ddg1210@gmail.com>
2024-06-12 09:55:51 -0400

    Added Oauth arguments in initial call

--- spotify_app.py
+++ spotify_app.py
@@ -13,7 +13,10 @@ def NumSavedSongs():
     # Set scope
     scope = "user-library-read playlist-read-private playlist-modify-public playlist-modify-private"
     # Authorize
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope,
+                         client_secret=client_secret,
+                         redirect_uri=redirect,
+                            client_id=client_ID))
     song = sp.current_user_saved_tracks(limit = 1, offset = 0, market = None)
     num_songs = song['total']
     return num_songs
